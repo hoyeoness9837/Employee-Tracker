07610aac1e25ca2a6f0dbc4fb66b7d563d296c0b
Hoyeon Kim <kim.hoyeon93@gmail.com>
2020-05-25 18:48:46 -0700

    change it to dynaically working

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ const viewAllEmployees = () => {
   db.query(
     `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary, employee.manager_id FROM employee LEFT JOIN role ON employee.role_id=role.id LEFT JOIN department ON role.department_id = department.id;`,
     (err, data) => {
-      if (err) {
-        console.log(err);
-      }
+      if (err) throw err;
       printTable(data);
       mainMenu();
     }
@@ -18,9 +16,7 @@ const viewAllEmployees = () => {
 // View department table of db
 const viewDepartment = () => {
   db.query('SELECT * FROM employment_db.department;', (err, data) => {
-    if (err) {
-      console.log(err);
-    }
+    if (err) throw err;
     printTable(data);
     mainMenu();
   });
@@ -28,9 +24,7 @@ const viewDepartment = () => {
 // View role table of db
 const viewRole = () => {
   db.query('SELECT * FROM employment_db.role;', (err, data) => {
-    if (err) {
-      console.log(err);
-    }
+    if (err) throw err;
     printTable(data);
     mainMenu();
   });
@@ -38,9 +32,7 @@ const viewRole = () => {
 // View employee tableof db
 const viewEmployee = () => {
   db.query('SELECT * FROM employment_db.employee;', (err, data) => {
-    if (err) {
-      console.log(err);
-    }
+    if (err) throw err;
     printTable(data);
     mainMenu();
   });
@@ -56,9 +48,7 @@ const addDepartment = () => {
     db.query(
       `INSERT INTO department (name) VALUES ('${answer.dptName} Department');`,
       (err, data) => {
-        if (err) {
-          console.log(err);
-        }
+        if (err) throw err;
         viewDepartment();
       }
     );
@@ -66,93 +56,117 @@ const addDepartment = () => {
 };
 // Add a new role row into db
 const addRole = () => {
-  prompt([
-    {
-      type: 'input',
-      name: 'title',
-      message: 'what role title do you want to add?',
-    },
-    {
-      type: 'input',
-      name: 'salary',
-      message: 'How much salary does this new role need?',
-    },
-    {
-      type: 'input',
-      name: 'dptId',
-      message: 'what is the department id for this new role?', // how to do it dynamically?
-    },
-  ]).then((answer) => {
-    db.query(
-      `INSERT INTO role (title, salary, department_id) VALUES ('${answer.title}', ${answer.salary}, ${answer.dptId});`,
-      (err, data) => {
-        if (err) {
-          console.log(err);
+  db.query('SELECT * FROM department', (err, result) => {
+    if (err) throw err;
+    departments = result.map((dep) => {
+      return { name: dep.name, value: dep.id };
+    });
+    prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'what role title do you want to add?',
+      },
+      {
+        type: 'input',
+        name: 'salary',
+        message: 'How much salary does this new role need?',
+      },
+      {
+        type: 'list',
+        name: 'dptId',
+        message: 'what is the department id for this new role?',
+        choices: departments,
+      },
+    ]).then((answer) => {
+      db.query(
+        `INSERT INTO role (title, salary, department_id) VALUES ('${answer.title}', ${answer.salary}, ${answer.dptId});`,
+        (err, data) => {
+          if (err) throw err;
+          viewRole();
         }
-        viewRole();
-      }
-    );
+      );
+    });
   });
 };
+
 // Add a new employee into db
 const addEmployee = () => {
-  prompt([
-    {
-      type: 'input',
-      name: 'fName',
-      message: 'what is the first name of this new employee?',
-    },
-    {
-      type: 'input',
-      name: 'lName',
-      message: 'what is the last name of this new employee?',
-    },
-    {
-      type: 'input',
-      name: 'roleId',
-      message: 'what role id is for this new employee?', // how to do it dynamically?
-    },
-    // {
-    //   type: 'input',
-    //   name: 'managerId',
-    //   message: 'what manager id is for this new employee?', // how to do it dynamically?
-    // },
-  ]).then((answer) => {
-    db.query(
-      `INSERT INTO employee (first_name, last_name, role_id) 
-      VALUES("${answer.fName}", "${answer.lName}", ${answer.roleId});`,
-      (err, data) => {
-        if (err) {
-          console.log(err);
+  db.query('SELECT * FROM role', (err, result) => {
+    if (err) throw err;
+    let role_ids = result.map((role) => {
+      return { name: role.title, value: role.id };
+    });
+    prompt([
+      {
+        type: 'input',
+        name: 'fName',
+        message: 'what is the first name of this new employee?',
+      },
+      {
+        type: 'input',
+        name: 'lName',
+        message: 'what is the last name of this new employee?',
+      },
+      {
+        type: 'list',
+        name: 'roleId',
+        message: 'what role id is for this new employee?',
+        choices: role_ids,
+      },
+      {
+        type: 'list',
+        name: 'managerId',
+        message: 'what manager id is for this new employee?',
+        choices: [1, 2, 3, 4, { name: 'None', value: 'NULL' }],
+      },
+    ]).then((answer) => {
+      db.query(
+        `INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+        VALUES("${answer.fName}", "${answer.lName}", ${answer.roleId}, ${answer.managerId});`,
+        (err, data) => {
+          if (err) throw err;
+          viewEmployee();
         }
-        viewEmployee();
-      }
-    );
+      );
+    });
   });
 };
 // Update a role by title
 const updateRole = () => {
-  prompt([
-    {
-      type: 'input',
-      name: 'title',
-      message: 'what role title do you want to update?', // how to do it dynamically, select from list
-    },
-    {
-      type: 'input',
-      name: 'salary',
-      message: 'How much salary does this updating role need?',
-    },
-  ]).then((answer) => {
-    db.query(
-      `UPDATE role SET salary = ${answer.salary} WHERE title = '${answer.title}';`,
-      (err, data) => {
-        if (err) {
-          console.log(err);
-        }
-        viewRole();
-      }
-    );
+  db.query('SELECT * FROM employee', (err, result) => {
+    if (err) throw err;
+    let emp_name = result.map((emp) => {
+      return emp.first_name;
+    });
+    db.query('SELECT * FROM role', (err, result) => {
+      if (err) throw err;
+      let role_list = result.map((role) => {
+        return { name: role.title, value: role.id };
+      });
+      prompt([
+        {
+          type: 'list',
+          name: 'employee',
+          message: 'whose role do want to upate?',
+          choices: emp_name,
+        },
+        {
+          type: 'list',
+          name: 'newrole',
+          message: 'which role do you want to update with?',
+          choices: role_list,
+        },
+      ]).then((answer) => {
+        db.query(
+          `UPDATE employee SET role_id = ${answer.newrole} WHERE first_name = '${answer.employee}';`,
+          (err, data) => {
+            if (err) throw err;
+            viewEmployee();
+          }
+        );
+      });
+    });
   });
 };
 // Delete an employee by id
@@ -165,9 +179,7 @@ const deleteEmployee = () => {
     db.query(
       `DELETE FROM employee WHERE id = ${answer.empId};`,
       (err, data) => {
-        if (err) {
-          console.log(err);
-        }
+        if (err) throw err;
         viewEmployee();
       }
     );
